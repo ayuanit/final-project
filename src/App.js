141e58a5b2de0f6998b47c7ab5bfea1ee1bcf41a
ayuanit <ayuuanitaa605@gmail.com>
2022-03-28 15:36:12 +0700

    module2aession3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import data from './file/file.js'
-import {AlbumImage,AlbumDesc, AlbumName, ButtonUrl} from './komponen/index.js';
+import ObjectLoop from './komponen/track.js'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
-      </div>
-      <div>
-        <h2>{data.name}</h2>
-        <h3>{data.album.name}</h3>
-        <p>{data.artists[0].name}</p>
-	<button><a href={data.album.artists[0].external_urls.spotify}>select</a></button>
-      </div>
+     <ObjectLoop/>
     </div>
   );
 }
